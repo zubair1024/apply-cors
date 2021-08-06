e0c3b1bdd627adffdddeb3bf0c54317fb6b3c213
Zubair Ahmed <zubair1024@gmail.com>
2021-08-06 22:18:56 +0530

    minor change

--- index.js
+++ index.js
@@ -11,8 +11,28 @@ const cors_proxy = require("cors-anywhere");
 cors_proxy
   .createServer({
     originWhitelist: whitelist,
+    // originBlacklist: originBlacklist,
     requireHeader: ["origin", "x-requested-with"],
-    removeHeaders: ["cookie", "cookie2"],
+    // checkRateLimit: checkRateLimit,
+    removeHeaders: [
+      "cookie",
+      "cookie2",
+      // Strip Heroku-specific headers
+      "x-request-start",
+      "x-request-id",
+      "via",
+      "connect-time",
+      "total-route-time",
+      // Other Heroku added debug headers
+      // 'x-forwarded-for',
+      // 'x-forwarded-proto',
+      // 'x-forwarded-port',
+    ],
+    redirectSameOrigin: true,
+    httpProxyOptions: {
+      // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+      xfwd: false,
+    },
   })
   .listen(port, host, function () {
     console.log("Running on " + host + ":" + port);
